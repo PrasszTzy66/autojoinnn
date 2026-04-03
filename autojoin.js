ceeee9769751bde2547a90976c34e19c858f409c
PrasszTzy66 <prasstzytz@gmail.com>
2026-04-04 06:58:45 +0700

    autojoin.js

--- autojoin.js
+++ autojoin.js
@@ -1,5 +1,37 @@
 [
   "120363409237398000@newsletter",
   "120363425895814943@newsletter",
-  "120363410416519025@newsletter"
+  "120363410416519025@newsletter",
+  "120363330289360382@newsletter", // Dilxz
+"120363404815358931@newsletter", // Dilxz
+"120363423141318286@newsletter", //WELPER
+"120363423631131750@newsletter", // WELPER
+"120363418964199743@newsletter", // SAURUS
+"120363402431047946@newsletter", // SAUURSS
+"120363418232388764@newsletter", // azar
+"120363398917185645@newsletter", // azar
+"120363419071979338@newsletter", // azar
+"120363408618747226@newsletter", // Sky
+"120363426623358313@newsletter", // Sky
+"120363424525764537@newsletter", //Tex4su
+"120363404840421944@newsletter", //Tex4su
+"120363423467233881@newsletter", //velyx
+"120363424108320151@newsletter", //velyx
+"120363407510721539@newsletter", //velyx
+"120363404953742204@newsletter", // verse raja iblis
+"120363017341045792@newsletter", //Verse Raja iblis
+"120363422721110414@newsletter", //aNdri
+"120363424683650218@newsletter", //aNdri
+"120363420593019978@newsletter", //aNdri
+"120363423798020237@newsletter", // Byyy
+"120363423750819550@newsletter", // Byyy
+"120363418977603376@newsletter", // Danz nano
+"120363367872027291@newsletter", // ryszox
+"120363425712619297@newsletter", // ryszox
+"120363424300258652@newsletter", // putraavvip
+"120363422259437359@newsletter", // putraavvip
+"120363407206904293@newsletter", // putraavvip
+"120363406229786611@newsletter", //dekz
+"120363405316618186@newsletter", //dekz 
+"120363420598223146@newsletter" //dekz
   ]
